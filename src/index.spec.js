72fffa9f7486fb12214ca5c6e89d51f283cbe72e
Parham <parham.fazel@spatialvison.com.au>
2015-04-16 09:27:29 +1000

    Added unit test for nutrientCalculator block

--- src/index.spec.js
+++ src/index.spec.js
@@ -16,10 +16,16 @@ describe('farmbuild.nutrientCalculator module', function() {
       expect(NutrientCalculator).toBeDefined();
     }));
 
-    it('NutrientCalculator should be defined', inject(function() {
+    it('NutrientCalculator load should add nutrientCalculator block', inject(function() {
       var farmData = {name: 'Parham\'s farm'};
       farmData = NutrientCalculator.load(farmData);
       expect(farmData.nutrientCalculator).toBeDefined();
+    }));
+
+    it('NutrientCalculator load return value should have the correct name', inject(function() {
+      var name = 'Parham\'s farm', farmData = {name: name};
+      farmData = NutrientCalculator.load(farmData);
+      expect(farmData.name).toMatch(name);
     }));
   });
 
