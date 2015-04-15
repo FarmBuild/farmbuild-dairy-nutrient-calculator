2b017cb0268339af3ec4678665b2850a0e532d9f
Parham <parham.fazel@spatialvison.com.au>
2015-04-15 17:05:31 +1000

    Added file upload for calculator example

--- examples/angularjs/index.js
+++ examples/angularjs/index.js
@@ -1,21 +1,23 @@
 angular.module('farmbuild.nutrientCalculator.examples', ['farmbuild.nutrientCalculator'])
 
-	.run(function($rootScope){
+	.run(function ($rootScope) {
 		$rootScope.appVersion = farmbuild.examples.nutrientcalculator.version;
 		$rootScope.decimalPrecision = farmbuild.examples.nutrientcalculator.decimalPrecision;
 	})
 
-	.controller('FarmCtrl', function ($scope, $http, NutrientCalculator) {
+	.controller('FarmCtrl', function ($scope, NutrientCalculator) {
 
 		$scope.farmData = {};
 
-		$scope.load = function() {
-			$http.get('../farmdata-susan.json').then(function(resp){
-				$scope.farmData = NutrientCalculator.load(resp.data);
-			})
-		}
+		$scope.load = function ($fileContent) {
+			try {
+				$scope.farmData = NutrientCalculator.load(angular.fromJson($fileContent));
+			} catch(e){
+				console.error('farmbuild.nutrientCalculator.examples > load: Your file should be in json format')
+			}
+		};
 
-		$scope.milkSold = function() {
+		$scope.milkSold = function () {
 			$scope.farmData.nutrientCalculator.milkSold =
 				NutrientCalculator.milkSold.nutrientOfMilkSoldByPercent(
 					$scope.farmData.nutrientCalculator.milkSold.milkSoldPerYearInLitre,
@@ -23,4 +25,32 @@ angular.module('farmbuild.nutrientCalculator.examples', ['farmbuild.nutrientCalc
 					$scope.farmData.nutrientCalculator.milkSold.milkFatPercentage)
 		}
 
+		$scope.exportFarmData = function(farmData){
+			var url = 'data:text/json;charset=utf8,' + encodeURIComponent(angular.toJson(farmData));
+			window.open(url, '_blank');
+			window.focus();
+		}
+
+	})
+
+	.directive('onReadFile', function ($parse) {
+		return {
+			restrict: 'A',
+			scope: false,
+			link: function (scope, element, attrs) {
+				var fn = $parse(attrs.onReadFile);
+
+				element.on('change', function (onChangeEvent) {
+					var reader = new FileReader();
+
+					reader.onload = function (onLoadEvent) {
+						scope.$apply(function () {
+							fn(scope, {$fileContent: onLoadEvent.target.result});
+						});
+					};
+
+					reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+				});
+			}
+		};
 	});
