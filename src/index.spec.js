528f15ebb5d375dd803f742b8ffb7c355eb16e7b
Parham <parham.fazel@spatialvison.com.au>
2015-04-17 09:16:07 +1000

    Completed unit tests for milkSold

--- src/index.spec.js
+++ src/index.spec.js
@@ -2,34 +2,43 @@
 
 describe('farmbuild.nutrientCalculator module', function() {
 
- // instantiate service
+ // define NutrientCalculator service
   var NutrientCalculator;
 
+  // instantiate farmbuild.nutrientCalculator module
   beforeEach(module('farmbuild.nutrientCalculator'));
 
+  // instantiate NutrientCalculator service
   beforeEach(inject(function (_NutrientCalculator_) {
     NutrientCalculator = _NutrientCalculator_;
   }));
 
   describe('NutrientCalculator factory', function(){
-    it('NutrientCalculator should be defined', inject(function() {
+
+    it('NutrientCalculator factory should be defined', inject(function() {
       expect(NutrientCalculator).toBeDefined();
     }));
 
-    it('NutrientCalculator load should add nutrientCalculator block', inject(function() {
+    it('window.farmbuild name space should be defined', inject(function() {
+      expect(window.farmbuild).toBeDefined();
+    }));
+
+    it('NutrientCalculator.milkSold should be defined', inject(function() {
+      expect(NutrientCalculator.milkSold).toBeDefined();
+    }));
+
+    it('NutrientCalculator.load should add nutrientCalculator block', inject(function() {
       var farmData = {name: 'Parham\'s farm'};
       farmData = NutrientCalculator.load(farmData);
       expect(farmData.nutrientCalculator).toBeDefined();
     }));
 
-    it('NutrientCalculator load return value should have the correct name', inject(function() {
+    it('NutrientCalculator.load return value should have the correct name', inject(function() {
       var name = 'Parham\'s farm', farmData = {name: name};
       farmData = NutrientCalculator.load(farmData);
       expect(farmData.name).toMatch(name);
     }));
 
   });
-
-
-
+  
 });
