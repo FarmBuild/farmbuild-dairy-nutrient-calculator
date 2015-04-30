b6c3eb470abf848ba45b2ed5aae33c3507548888
hxg <hiroki.gota@spatialvision.com.au>
2015-04-30 17:36:19 +1000

    multi

--- src/fertilizer-purchased/index.spec.js
+++ src/fertilizer-purchased/index.spec.js
@@ -92,13 +92,35 @@ describe('fertilizerPurchased module', function() {
       expect(result.sulphurPercentage).toEqual(0.3)
     }));
 
+    it('Dairy manure stockpile type and the weight of 4000kg with wet weight', inject(function() {
+      var weight = 13000,
+        fertilizer = addFertilizer(superphosphate, weight, false);
+      $log.info('fertilizer: %j', fertilizer);
+      var result = fertilizerPurchased.calculate(fertilizer)
+
+      $log.info('result generated: %j', result);
+
+      expect(result.weight).toEqual(13000)
+      expect(result.dryMatterWeight).toEqual(13000)
+      expect(result.nitrogenPercentage).toEqual(0)
+      expect(result.nitrogenInKg).toEqual(0)
+      expect(between(result.phosphorusInKg,1144,1144.1)).toBeTruthy()
+      expect(between(result.phosphorusPercentage,8.8,8.89)).toBeTruthy()
+
+      expect(result.potassiumInKg).toEqual(0)
+      expect(result.potassiumPercentage).toEqual(0)
+      expect(result.sulphurInKg).toEqual(1430)
+      expect(result.sulphurPercentage).toEqual(11)
+    }));
+
     it('DAP, Super and Urea type and the weight of 4000kg, 13000kg, 11000kg', inject(function() {
-      var weightDap = 4000, weightSuperphosphate = 13000,
+      var weightDairyManureStockpile = 4000, weightSuperphosphate = 13000,
         weightUrea = 11000,
         fertilizers = fertilizerPurchased
-          .add(dap, weightDap, true)
+          .add(dairyManureStockpile, weightDairyManureStockpile, true)
           .add(superphosphate, weightSuperphosphate, true)
-          .add(urea, weightUrea, true).fertilizers()
+          .add(urea, weightUrea, true)
+          .fertilizers()
 
       $log.info('fertilizers created: %j', fertilizers)
 
@@ -108,16 +130,16 @@ describe('fertilizerPurchased module', function() {
 
       $log.info('result generated: %j', result);
 
-//      expect(result.weight).toEqual(4000)
-//      expect(result.dryMatterWeight).toEqual(4000)
-//      expect(result.nitrogenPercentage).toEqual(18)
-//      expect(result.nitrogenInKg).toEqual(720)
-//      expect(result.phosphorusInKg).toEqual(800)
-//      expect(result.phosphorusPercentage).toBe(20)
-//      expect(result.potassiumInKg).toEqual(0)
-//      expect(result.potassiumPercentage).toEqual(0)
-//      expect(result.sulphurInKg).toEqual(64)
-//      expect(result.sulphurPercentage).toEqual(1.6)
+      expect(result.weight).toEqual(28000)
+      expect(result.dryMatterWeight).toEqual(28000)
+      expect(result.nitrogenInKg).toEqual(5117.6)
+      expect(result.nitrogenPercentage).toEqual(18.27714285714286)
+      expect(result.phosphorusInKg).toEqual(1166.0000000000002)
+      expect(result.phosphorusPercentage).toBe(4.164285714285715)
+      expect(result.potassiumInKg).toEqual(55.2)
+      expect(result.potassiumPercentage).toEqual(0.19714285714285715)
+      expect(result.sulphurInKg).toEqual(1442)
+      expect(result.sulphurPercentage).toEqual(5.1499999999999995)
     }));
 
   });
