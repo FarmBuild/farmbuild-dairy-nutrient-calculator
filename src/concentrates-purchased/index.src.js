ad1ae0bc2f09956750e902cca46432ada05920a0
hxg <hiroki.gota@spatialvision.com.au>
2015-05-04 17:36:53 +1000

    concentrate part 1

---
+++ src/concentrates-purchased/index.src.js
@@ -0,0 +1,103 @@
+/**
+ * @since 0.0.1
+ * @copyright 2015 Spatial Vision, Inc. http://spatialvision.com.au
+ * @license The MIT License
+ * @author Spatial Vision
+ * @version 0.1.0
+ */
+
+'use strict';
+
+/**
+ * nutrientCalculator/concentratesPurchased singleton
+ * @module nutrientCalculator/concentratesPurchased
+ */
+angular.module('farmbuild.nutrientCalculator')
+
+  .factory('concentratesPurchased',
+  function (validations, concentrateDefaults, concentrateTypes, concentrateValidator, concentrateCalculator,
+            collections,
+            $log) {
+
+    var concentratesPurchased = {types:concentrateTypes, calculator:concentrateCalculator},
+      _concentrates  = [],
+      calculator = concentrateCalculator,
+      validator = concentrateValidator;
+    
+    /**
+     * Removes the concentrate  at specified index
+     * @method removeAt
+     * @returns {object} concentratesPurchased
+     * @public
+     * @static
+     */
+    function _removeAt(index) {
+      $log.info('removing concentrate  at index ' + index);
+
+      collections.removeAt(_concentrates , index);
+
+      return concentratesPurchased;
+    };
+    concentratesPurchased.removeAt = _removeAt;
+
+    /**
+     * Returns the current instance of concentrates  purchased
+     * @method add
+     * @param {!type} type - name of new type, can only contain alphanumeric values with space or underscore but no other special characters
+     * @param {!number} weight - value must be > 0
+     * @param {!boolean} isDry -true if the concentrate  is dry, false if it's wet
+     * @returns {object} concentratesPurchased
+     * @public
+     * @static
+     */
+    concentratesPurchased.concentrates  = function() { return _concentrates };
+
+    function _create(type, weight, isDry) {
+      return {type: type, weight:weight, isDry:isDry};
+    }
+    concentratesPurchased.create = _create;
+
+    /**
+     * Adds a new concentrate  for nutrient calculation
+     * @method add
+     * @param {!type} type - name of new type, can only contain alphanumeric values with space or underscore but no other special characters
+     * @param {!number} weight - value must be > 0
+     * @param {!boolean} isDry -true if the concentrate  is dry, false if it's wet
+     * @returns {object} concentratesPurchased
+     * @public
+     * @static
+     */
+    function _add(type, weight, isDry) {
+      var concentrate  = _create(type, weight, isDry);
+      $log.info('concentratesPurchased.add concentrate  ...', concentrate );
+      if (!validator.validate(concentrate )) {
+        $log.error('concentratesPurchased.add unable to add as the validation has been failed');
+        return undefined;
+      }
+      collections.add(_concentrates , concentrate );
+      return concentratesPurchased;
+    };
+    concentratesPurchased.add = _add;
+
+    concentratesPurchased.asArray = function() { return _concentrates };
+
+    /**
+     * Calculates the nutrient of the concentrates  purchased
+     * @method calculate
+     * @param concentrates
+     * @returns {object} the result of the concentrates  purchased
+     * @public
+     * @static
+     */
+    concentratesPurchased.calculate = function(concentrates ) {
+      $log.info('concentratesPurchased.calculate...');
+      if(!validator.validateAll(concentrates )) {
+        $log.error('concentratesPurchased.calculate invalid concentrates , see the error above and fix based on API...');
+        return undefined;
+      }
+
+      return calculator.calculate(concentrates );
+    }
+
+    return concentratesPurchased;
+  });
---
+++ src/concentrates-purchased/types.spec.js
@@ -0,0 +1,71 @@
+'use strict';
+
+describe('farmbuild.nutrientCalculator module', function() {
+
+  // instantiate service
+  var $log;
+  var concentrateTypes, dairyManureStockpileName = 'Dairy manure stockpile',
+    dapName = 'DAP', superphosphate = 'Superphosphate (Super)';
+
+  beforeEach(module('farmbuild.nutrientCalculator'));
+
+  beforeEach(inject(function (_$log_, _concentrateTypes_) {
+    $log = _$log_;
+    concentrateTypes = _concentrateTypes_;
+  }));
+
+  describe('concentrateTypes factory', function(){
+    it('concentrateTypes should be defined', inject(function() {
+      expect(concentrateTypes).toBeDefined();
+    }));
+  });
+
+//  describe('types.at', function(){
+//
+//    it('given default types, at(0) should return ' + dairyManureStockpileName, inject(function() {
+//      var dairyManureStockpile = concentrateTypes.at(0);
+//      expect(dairyManureStockpile.name).toEqual(dairyManureStockpileName);
+//    }));
+//
+//    it('given default types, at(1) should return ' + dapName, inject(function() {
+//      var dap = concentrateTypes.at(1);
+//      expect(dap.name).toEqual(dapName);
+//      expect(dap.nitrogenPercentage).toEqual(18);
+//      expect(dap.phosphorusPercentage).toEqual(20);
+//      expect(dap.potassiumPercentage).toEqual(0);
+//      expect(dap.sulphurPercentage).toEqual(1.6);
+//      expect(dap.dryMatterPercentage).toEqual(100);
+//    }));
+//
+//  });
+//
+//  describe('types.add', function(){
+//    it('New concentrate  type should be added on top of the existing', inject(function() {
+//      var name = 'New Fertilizer Type 1', dryMatterPercentage = 0.8,
+//        nitrogenPercentage = 2, phosphorusPercentage = 0.3,
+//        potassiumPercentage = 2.1, sulphurPercentage = 0.4,
+//        countExisting = concentrateTypes.size(),
+//        result = concentrateTypes.add(name, dryMatterPercentage, sulphurPercentage, potassiumPercentage, phosphorusPercentage, nitrogenPercentage),
+//        newCount = concentrateTypes.size();
+//      expect(newCount-countExisting).toEqual(1);
+//      expect(concentrateTypes.last().name).toEqual(name);
+//    }));
+//  });
+//
+//  describe('types.byName', function(){
+//    it('byName should find ' + superphosphate, inject(function() {
+//      var found = concentrateTypes.byName(superphosphate);
+//      expect(found.name).toEqual(superphosphate);
+//    }));
+//  });
+//
+//  describe('types.removeAt', function(){
+//    it('removeAt should remove by index ', inject(function() {
+//      var types = concentrateTypes.defaultTypes(),
+//        removed = concentrateTypes.removeAt(0);
+//      expect(removed.length).toEqual(types.length -1);
+//    }));
+//  });
+
+});
+
