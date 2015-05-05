2d3ff72ffd0071156e8adfc6677974e36249c8ef
hxg <hiroki.gota@spatialvision.com.au>
2015-05-05 11:04:31 +1000

    finished api

--- src/concentrates-purchased/index.spec.js
+++ src/concentrates-purchased/index.spec.js
@@ -3,9 +3,10 @@ describe('concentratesPurchased  module', function() {
   // instantiate service
   var $log;
   var concentratesPurchased, pelletsCalf, pelletsCalfName = 'Pellets Calf',
-    dairyManureStockpile, dairyManureStockpileName = 'Dairy manure stockpile',
-    superphosphate, superphosphateName = 'Superphosphate (Super)',
-    urea, ureaName = 'Urea';
+    pelletsDairy,pelletsDairyName = 'Pellets Dairy',
+    averageConcentrate, averageConcentrateName = 'Average concentrate',
+    fruit,fruitName='Fruit'
+  ;
 
   beforeEach(module('farmbuild.nutrientCalculator', function($provide) {
     $provide.value('$log', console);
@@ -15,10 +16,14 @@ describe('concentratesPurchased  module', function() {
     $log = _$log_;
     concentratesPurchased  = _concentratesPurchased_;
     pelletsCalf =  concentratesPurchased.types.byName(pelletsCalfName);
-//    expect(superphosphate.name).toEqual(superphosphateName)
-//    urea =  concentratesPurchased.types.byName(ureaName);
-//    expect(urea.name).toEqual(ureaName)
+    expect(pelletsCalf.name).toEqual(pelletsCalfName);
+    pelletsDairy =  concentratesPurchased.types.byName(pelletsDairyName);
+    expect(pelletsDairy.name).toEqual(pelletsDairyName);
+    averageConcentrate =  concentratesPurchased.types.byName(averageConcentrateName);
+    fruit =  concentratesPurchased.types.byName(fruitName);
+    expect(fruit.name).toEqual(fruitName);
   }));
+
 
   describe('Given concentratesPurchased  factory', function(){
     it('concentratesPurchased  should be defined', inject(function() {
@@ -38,13 +43,13 @@ describe('concentratesPurchased  module', function() {
     return val >= min && val <= max;
   }
 
-  describe('Given the default value, calculate nutrient of concentrate purchased', function(){
-    it('DAP type with undefined amount should fail', inject(function() {
+  describe('Given the default value, calculate the nutrient of concentrate purchased', function(){
+    it(pelletsCalfName + ' type with undefined amount should fail', inject(function() {
       var result = concentratesPurchased.calculate(addConcentrate(pelletsCalf, true))
       expect(result).toBeUndefined()
     }));
 
-    it('DAP type and the weight of 4000kg should calculate', inject(function() {
+    it(pelletsCalfName + ' type and the weight of 4000kg should calculate', inject(function() {
       var weight = 1850,
         concentrate = addConcentrate(pelletsCalf, weight, true);
       $log.info('concentrate: %j', concentrate);
@@ -62,79 +67,49 @@ describe('concentratesPurchased  module', function() {
       expect(result.potassiumPercentage).toEqual(0.84)
       expect(result.sulphurInKg).toEqual(5.18)
       expect(result.sulphurPercentage).toEqual(0.27999999999999997)
-      expect(result.metabolisableEnergyInMJ).toEqual(2353)
+      expect(result.metabolisableEnergyInMJ).toEqual(23532)
       expect(result.metabolisableEnergyInMJPerKg).toEqual(12.72)
     }));
 
-//    it('Dairy manure stockpile type and the weight of 4000kg with wet weight', inject(function() {
-//      var weight = 4000,
-//        concentrate = addConcentrate(dairyManureStockpile, weight, false);
-//      $log.info('concentrate: %j', concentrate);
-//      var result = concentratesPurchased.calculate(concentrate)
-//
-//      $log.info('result generated: %j', result);
-//
-//      expect(result.weight).toEqual(4000)
-//      expect(between(result.dryMatterWeight, 3073.2, 3073.3)).toBeTruthy()
-//      expect(result.nitrogenPercentage).toEqual(1.44)
-//      expect(between(result.nitrogenInKg, 44.254, 44.255)).toBeTruthy()
-//      expect(between(result.phosphorusInKg,16.9026,17.0)).toBeTruthy()
-//      expect(result.phosphorusPercentage).toBe(0.55)
-//      expect(result.potassiumInKg).toEqual(42.41016)
-//      expect(result.potassiumPercentage).toEqual(1.38)
-//      expect(result.sulphurInKg).toEqual(9.2196)
-//      expect(result.sulphurPercentage).toEqual(0.3)
-//    }));
-//
-//    it('Dairy manure stockpile type and the weight of 4000kg with wet weight', inject(function() {
-//      var weight = 13000,
-//        concentrate = addConcentrate(superphosphate, weight, false);
-//      $log.info('concentrate: %j', concentrate);
-//      var result = concentratesPurchased.calculate(concentrate)
-//
-//      $log.info('result generated: %j', result);
-//
-//      expect(result.weight).toEqual(13000)
-//      expect(result.dryMatterWeight).toEqual(13000)
-//      expect(result.nitrogenPercentage).toEqual(0)
-//      expect(result.nitrogenInKg).toEqual(0)
-//      expect(between(result.phosphorusInKg,1144,1144.1)).toBeTruthy()
-//      expect(between(result.phosphorusPercentage,8.8,8.89)).toBeTruthy()
-//
-//      expect(result.potassiumInKg).toEqual(0)
-//      expect(result.potassiumPercentage).toEqual(0)
-//      expect(result.sulphurInKg).toEqual(1430)
-//      expect(result.sulphurPercentage).toEqual(11)
-//    }));
-//
-//    it('DAP, Super and Urea type and the weight of 4000kg, 13000kg, 11000kg', inject(function() {
-//      var weightDairyManureStockpile = 4000, weightSuperphosphate = 13000,
-//        weightUrea = 11000,
-//        concentrates = concentratesPurchased
-//          .add(dairyManureStockpile, weightDairyManureStockpile, true)
-//          .add(superphosphate, weightSuperphosphate, true)
-//          .add(urea, weightUrea, true)
-//          .concentrates()
-//
-//      $log.info('concentrates created: %j', concentrates)
-//
-//      expect(angular.isArray(concentrates)).toBeTruthy()
-//
-//      var result = concentratesPurchased.calculate(concentrates)
-//
-//      $log.info('result generated: %j', result);
-//
-//      expect(result.weight).toEqual(28000)
-//      expect(result.dryMatterWeight).toEqual(28000)
-//      expect(result.nitrogenInKg).toEqual(5117.6)
-//      expect(result.nitrogenPercentage).toEqual(18.27714285714286)
-//      expect(result.phosphorusInKg).toEqual(1166.0000000000002)
-//      expect(result.phosphorusPercentage).toBe(4.164285714285715)
-//      expect(result.potassiumInKg).toEqual(55.2)
-//      expect(result.potassiumPercentage).toEqual(0.19714285714285715)
-//      expect(result.sulphurInKg).toEqual(1442)
-//      expect(result.sulphurPercentage).toEqual(5.1499999999999995)
-//    }));
+
+
+  });
+
+  describe('Given the default value, calculate the nutrient of concentrate purchased', function(){
+
+    it('DAP, Super and Urea type and the weight of 4000kg, 13000kg, 11000kg', inject(function() {
+      var weightPelletsCalf1=1850,weightPelletsDairy1 =8150,
+        weightPelletsDairy2 = 2050,weightAverageConcentrate1=7815,weightPelletsCalf2=1012,
+        weightPelletsDairy3=9658,weightPelletsDairy4=8571,weightFruit=328;
+        concentrates = concentratesPurchased
+          .add(pelletsCalf, weightPelletsCalf1, true)
+          .add(pelletsDairy, weightPelletsDairy1, true)
+          .add(pelletsDairy, weightPelletsDairy2, true)
+          .add(averageConcentrate, weightAverageConcentrate1, true)
+          .add(pelletsCalf, weightPelletsCalf2, true)
+          .add(pelletsDairy, weightPelletsDairy3, true)
+          .add(pelletsDairy, weightPelletsDairy4, true)
+          .add(fruit, weightFruit, true)
+          .concentrates()
+      expect(angular.isArray(concentrates)).toBeTruthy()
+
+      var result = concentratesPurchased.calculate(concentrates)
+
+      $log.info('result generated: %j', result);
+
+      expect(result.weight).toEqual(39434)
+      expect(result.dryMatterWeight).toEqual(39434)
+      expect(result.nitrogenInKg).toEqual(1075.8433)
+      expect(result.nitrogenPercentage).toEqual(2.728212456256023)
+      expect(result.phosphorusInKg).toEqual(275.5202)
+      expect(result.phosphorusPercentage).toBe(0.6986869199168231)
+      expect(result.potassiumInKg).toEqual(292.8796)
+      expect(result.potassiumPercentage).toEqual(0.7427083227671551)
+      expect(result.sulphurInKg).toEqual(101.94000000000001)
+      expect(result.sulphurPercentage).toEqual(0.25850788659532387)
+      expect(result.metabolisableEnergyInMJ).toEqual(495334.39999999997)
+      expect(result.metabolisableEnergyInMJPerKg).toEqual(11.6)
+    }));
 
   });
 
